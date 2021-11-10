d217bb090e5273aa0e42ac23009fff2d93ac1b85
raduanakhawla <raduanakhawla468@gmail.com>
2021-11-10 10:21:22 +0600

    doctors-portal

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyBYPFjZldMC2ZVpe7rNzTM0gnid1M9aBHc",
+        authDomain: "doctors-portal-module73.firebaseapp.com",
+        projectId: "doctors-portal-module73",
+        storageBucket: "doctors-portal-module73.appspot.com",
+        messagingSenderId: "389374439073",
+        appId: "1:389374439073:web:14a83206e5a3807d580ae0"
+};
+export default firebaseConfig;
+
+// apiKey: process.env.REACT_APP_API_KEY,
+// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// projectId: process.env.REACT_APP_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_APP_ID,
